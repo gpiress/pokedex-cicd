6fa03193ace7ec34efbf8903463ef1a99f9c157a
gpiress <gpires@spotify.com>
2022-12-19 19:31:17 +0100

    Initial routes

---
+++ pokedex.mjs
@@ -0,0 +1,14 @@
+import { readFileSync } from 'fs';
+
+const pokemon = JSON.parse(readFileSync('./data/pokemon.json', { encoding: 'utf8' }));
+
+export const all = () => {
+    return pokemon;
+};
+
+export const byType = (desiredType) => {
+    const filtered = pokemon
+        .filter((pkmn) => pkmn.type === desiredType);
+
+    return filtered;
+};
--- server.mjs
+++ server.mjs
@@ -1,12 +1,24 @@
 import express from 'express';
 
+import { all, byType } from './pokedex.mjs';
 
 const PORT = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
     res.send('Hello world!');
+});
+
+app.get('/pokemon', (_, res) => {
+    const allPokemon = all();
+    res.send(allPokemon);
+});
+
+app.get('/pokemon/:type', (req, res) => {
+    const { type } = req.params;
+    const filteredPokemon = byType(type);
+    res.send(filteredPokemon);
 });
 
 app.listen(PORT, () => {
